3f4918737300983aef26e8fc3e752c764ae0b75e
Nick von Pentz <nickvonpentz@gmail.com>
2020-09-10 14:23:14 -0600

    Make modifications to use a Lambda wrapper
    Previously the action would start a CodeBuild project.  The
    modifications in this commit instead would start a Lambda function that
    starts a CodeBuild projet and returns it's ID.

--- code-build.js
+++ code-build.js
@@ -27,8 +27,18 @@ function runBuild() {
 }
 
 async function build(sdk, params) {
-  // Start the build
-  const start = await sdk.codeBuild.startBuild(params).promise();
+  // Invoke the lambda to start the build
+  const lambdaParams = {
+    FunctionName: "GeneralDockerBuildPipelineLambdaFunction",
+    Payload: JSON.stringify({
+      owner: params.owner,
+      repo: params.repo,
+      branch: params.branch,
+      sourceVersion: params.sourceVersion
+    })
+  }
+  const response = await sdk.lambda.invoke(lambdaParams).promise();
+  const start = JSON.parse(JSON.parse(response.Payload))
 
   // Wait for the build to "complete"
   return waitForBuildEndTime(sdk, start.build);
@@ -109,8 +119,10 @@ async function waitForBuildEndTime(sdk, { id, logs }, nextToken) {
 }
 
 function githubInputs() {
-  const projectName = core.getInput("project-name", { required: true });
-  const { owner, repo } = github.context.repo;
+  let { owner, repo } = github.context.repo;
+
+  const branch = process.env[`GITHUB_REF`].split("/")[2]
+
   const { payload } = github.context;
   // The github.context.sha is evaluated on import.
   // This makes it hard to test.
@@ -124,63 +136,44 @@ function githubInputs() {
       : process.env[`GITHUB_SHA`];
 
   assert(sourceVersion, "No source version could be evaluated.");
-  const buildspecOverride =
-    core.getInput("buildspec-override", { required: false }) || undefined;
-
-  const envPassthrough = core
-    .getInput("env-vars-for-codebuild", { required: false })
-    .split(",")
-    .map((i) => i.trim())
-    .filter((i) => i !== "");
 
   return {
-    projectName,
     owner,
     repo,
+    branch,
     sourceVersion,
-    buildspecOverride,
-    envPassthrough,
   };
 }
 
 function inputs2Parameters(inputs) {
   const {
-    projectName,
     owner,
     repo,
+    branch,
     sourceVersion,
-    buildspecOverride,
-    envPassthrough = [],
   } = inputs;
-
-  const sourceTypeOverride = "GITHUB";
-  const sourceLocationOverride = `https://github.com/${owner}/${repo}.git`;
-
-  const environmentVariablesOverride = Object.entries(process.env)
-    .filter(
-      ([key]) => key.startsWith("GITHUB_") || envPassthrough.includes(key)
-    )
-    .map(([name, value]) => ({ name, value, type: "PLAINTEXT" }));
 
   // The idempotencyToken is intentionally not set.
   // This way the GitHub events can manage the builds.
   return {
-    projectName,
-    sourceVersion,
-    sourceTypeOverride,
-    sourceLocationOverride,
-    buildspecOverride,
-    environmentVariablesOverride,
+    owner,
+    repo,
+    branch,
+    sourceVersion
   };
 }
 
 function buildSdk() {
   const codeBuild = new aws.CodeBuild({
-    customUserAgent: "aws-actions/aws-codebuild-run-build",
+    customUserAgent: "brave-intl/aws-codebuild-run-build",
   });
 
   const cloudWatchLogs = new aws.CloudWatchLogs({
-    customUserAgent: "aws-actions/aws-codebuild-run-build",
+    customUserAgent: "brave-intl/aws-codebuild-run-build",
+  });
+
+  const lambda = new aws.Lambda({
+    customUserAgent: "brave-intl/aws-codebuild-run-build",
   });
 
   assert(
@@ -188,7 +181,7 @@ function buildSdk() {
     "No credentials. Try adding @aws-actions/configure-aws-credentials earlier in your job to set up AWS credentials."
   );
 
-  return { codeBuild, cloudWatchLogs };
+  return { codeBuild, cloudWatchLogs, lambda };
 }
 
 function logName(Arn) {
